eb26af461fb4e2945dccd8d8048b7a731121cf02
Emroze <emrozesd@users.noreply.github.com>
2019-02-08 21:26:16 -0800

    add more style and links

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,22 +1,17 @@
+$(document).ready(function(){
 
-function scroll_to(clicked_link, nav_height) {
-    var element_class = clicked_link.attr('href').replace('#', '.');
-    var scroll_to = 0;
-    if(element_class != '.top-content') {
-        element_class += '-container';
-        scroll_to = $(element_class).offset().top - nav_height;
-    }
-    if($(window).scrollTop() != scroll_to) {
-        $('html, body').stop().animate({scrollTop: scroll_to}, 1000);
-    }
-}
- 
-jQuery(document).ready(function() {
-
-    $('a.scroll-link').on('click', function(e) {
-        e.preventDefault();
-        scroll_to($(this), $('nav').outerHeight());
-    });
- 
+  $('.phone').tooltip({title: "612-803-8289", trigger: "hover", placement: "bottom", animation: true});   
+  $('.mail').tooltip({title: "margy.shuster@gmail.com", trigger: "hover", placement: "bottom", animation: true}); 
 
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-65px";
+  }
+  prevScrollpos = currentScrollPos;
+}
